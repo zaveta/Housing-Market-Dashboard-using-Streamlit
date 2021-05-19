381522c9fdc195db345d417b4e2a3eb60ce2b687
Victoria Sharaf <54998579+zaveta@users.noreply.github.com>
2021-05-19 20:41:07 +0000

    Adding Load in real time feature

--- dashboard.py
+++ dashboard.py
@@ -5,68 +5,95 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sqlalchemy import create_engine
+from bs4 import BeautifulSoup
+import requests
 
-database_filepath = './Monthly_Stat_Reports.db'
-engine = create_engine('sqlite:///{}'.format(database_filepath))
-sqlite_connection = engine.connect()
+import download_data as dd
 
-cities_list = pd.read_sql_query('''
-                                SELECT DISTINCT City FROM Monthly_Stat_Reports_Cities
-                                ''', engine)['City'].to_list()
+city_codes_df = pd.read_csv('city_codes.csv')
 
-years_list = pd.read_sql_query('''
-                                SELECT DISTINCT Year FROM Monthly_Stat_Reports_Cities
-                                ''', engine)['Year'].to_list()
-month_list = pd.read_sql_query('''
-                                SELECT DISTINCT Month FROM Monthly_Stat_Reports_Cities
-                                ''', engine)['Month'].to_list()
+columns = ['City', 'Townhouse-Condo Attached', 'Single-Family Detached', 
+           'New Listings', 'Pending Sales',
+           'Closed Sales', 'Days on Market Until Sale', 'Median Sales Price',
+           'Average Sales Price', 'Percent of Original List Price Received',
+           'Inventory of Homes for Sale', 'Year', 'Month', 'Month_num']
 
+df = pd.DataFrame(columns=columns)
 
-years_list.sort()
+num_to_month = {
+    '01': "January", 
+    '02': "February", 
+    '03': "March", 
+    '04': "April", 
+    '05': "May", 
+    '06': "June",
+    '07': "July", 
+    '08': "August", 
+    '09': "September", 
+    '10': "October", 
+    '11': "November", 
+    '12': "December"
+}
+
+month_to_num = {
+    "January": '01', 
+    "February": '02', 
+    "March": '03', 
+    "April": '04', 
+    "May": '05', 
+    "June": '06',
+    "July": '07', 
+    "August": '08', 
+    "September": '09', 
+    "October": '10', 
+    "November": '11', 
+    "December": '12'
+}
+
+# lists of cities, years and months
+cities_list = city_codes_df['city'].to_list()
+years_list = list(range(2011,2022))
+month_list = list(num_to_month.values())
+
 st.sidebar.header('Options')
 
-city = st.sidebar.selectbox("Choose city", (cities_list))
-
-years = st.sidebar.multiselect("Choose year", years_list, default=years_list[-1])
-
-months = st.sidebar.multiselect("Choose month", month_list, default="January")
+# coosen cities, years list and months
+choosen_city = st.sidebar.selectbox("Choose city", (cities_list))
+choosen_years = st.sidebar.multiselect("Choose year", years_list, default=years_list[-1])
+choosen_months = st.sidebar.multiselect("Choose month", month_list, default="January")
 
 house_type = st.sidebar.selectbox("Chose House Type", ("Single-Family Detached", "Townhouse-Condo Attached"))
 
-st.title(city)
+st.title(choosen_city)
 
-table = pd.read_sql_query(f'''SELECT *
-                              FROM Monthly_Stat_Reports_Cities
-                              WHERE City = "{city}" 
-                              AND "{house_type}" = 1
-                              ORDER BY Year ASC, Month DESC
-                              ''', engine)
+for year in choosen_years:
+    for mon_name in choosen_months:
+        df = dd.loadYearAndMonthData(choosen_city, city_codes_df, month_to_num[mon_name], year, df)
+
+choosen_df = df[df["Year"].isin(choosen_years) & \
+                df["Month"].isin(choosen_months) & \
+                df["City"].isin([choosen_city]) & \
+                df[house_type] == 1]
 
 
-choosen_df = table[table["Year"].isin(years) & table["Month"].isin(months)]
+choosen_df = choosen_df.sort_values("Month_num")
 
-choosen_df["Date"] = choosen_df["Year"].apply(str) + " " + choosen_df["Month"]
-choosen_df = choosen_df.sort_values("Month num")
-
-st.table(choosen_df[['Date', 'New Listings', 'Closed Sales',
+st.table(choosen_df[['Year', 'New Listings', 'Closed Sales',
        'Days on Market Until Sale', 'Median Sales Price',
        'Average Sales Price', 'Percent of Original List Price Received',
        'Inventory of Homes for Sale']].transpose())
 
-fig, ax = plt.subplots(figsize=(10,5))
-sns.barplot(data=choosen_df, x='Date', y='New Listings', ax=ax, palette='OrRd')
-plt.title("New Listings")
-plt.xticks(rotation=45)
-st.pyplot(fig)
+# fig, ax = plt.subplots(figsize=(10,5))
+# sns.barplot(data=choosen_df, x='Date', y='New Listings', ax=ax, palette='OrRd')
+# plt.title("New Listings")
+# plt.xticks(rotation=45)
+# st.pyplot(fig)
 
-fig, ax = plt.subplots(figsize=(10,5))
-sns.barplot(data=choosen_df, x='Date', y='Median Sales Price', ax=ax, palette='OrRd')
-plt.title("Median Sales Price")
-plt.xticks(rotation=45)
-st.pyplot(fig)
-
-sqlite_connection.close()
+# fig, ax = plt.subplots(figsize=(10,5))
+# sns.barplot(data=choosen_df, x='Date', y='Median Sales Price', ax=ax, palette='OrRd')
+# plt.title("Median Sales Price")
+# plt.xticks(rotation=45)
+# st.pyplot(fig)
 
 
 
